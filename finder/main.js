68eea01e6a4e000eff3b376a68f10a982a83b3ba
Hamit Özdemir <12991847+hamitozdemir@users.noreply.github.com>
2024-07-19 23:30:35 +0300

    Can fetch the XML and PMC IDs now
    Transferring ideas from https://github.com/hamitozdemir/email-finder-docker project,
    Since deciding to not use PubReader, I see no point in having a backend, because it's mostly XML manipulation in the end
    Will look into PDF (Other Formats) on PMC pages, as well as a possibility of pulling email with its parent, straight from the XML

--- finder/main.js
+++ finder/main.js
@@ -1,7 +1,24 @@
 
 search = () => {
-	parse_ncbi_result('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Fumihito%20Hirai[Author]+gastroenterology');
-	console.log('here be things');
+	//parse_ncbi_result('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Fumihito%20Hirai[Author]+gastroenterology');
+	let url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term=Fumihito%20Hirai[Author]+gastroenterology"
+	//console.log('here be things');
+	fetch(url)
+  .then((response) => response.text())
+  .then((text) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/xml");
+		let id_arr_html_col = doc.documentElement.childNodes[3].children
+
+		//let id_list = [].slice.call(id_arr_html_col);
+		let id_list = Array.from(id_arr_html_col);
+		console.log(id_list) // Id objects
+		console.log(id_list[0].textContent) // id as string stripped from <Id> </Id>
+		console.log(typeof(id_list[0])) // object
+		console.log(typeof(id_list[0].textContent)) // string
+  });
+
+
 };
 
 parse_ncbi_result = (url) => {
