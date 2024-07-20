b30ce9e8a26f537b2bf270a1e04b410106dab2e0
Hamit Özdemir <12991847+hamitozdemir@users.noreply.github.com>
2024-07-20 22:39:50 +0300

    Core functionality is pretty much done
    There are some extra cases that I will need to look into as it returns undefined for e-mails of contrib-lines on 3rd call for current example
    As well as getting orchid links as head and more undefined for rest of contrib-lines on that call
    As well as getting degrees, instead of e-mails on contrib-line-one-level for last call
    
    Perhaps the right call is to find e-mail child of the object, instead of creating all these special cases for each and every one, and just get innerHTML for the rest of the object
    
    Implement pending for clear function to reset everything to base, clearing current_ids[], current_cursor back to 1, clearing all divs
    Implement pending for re-try functionality in-case it fails to fetch the data
    
    Hold currently fetched IDs in a globally grouped array, as well as current cursor position in the group
    Require entering an author name before search, yet to implement author name to actual search, since it's like the most basic thing
    
    Put fetching IDs inside a try/catch, even implementing a handle_connection_error function, but it doesn't seem to throw an error, like at all when failing and catch block never gets executed, neither does the if block in handle_connection_error (noted down a possibly error on my part at line 201)
    
    Display the number of found articles, as well as the number of groups to the user
    Draw action buttons, which is just "Find" for now
    Pressing button fetches the group of IDs for the current cursor position and increments the cursor by one, if cursor reaches group length, disables button
    
    Get IDs joined together by ',' as arg for fetch mails
    Created a few custom cases for different set of e-mail formatting in XML, still get undefined results, so I will have to look into fine-tuning that
    Added 2 extra arrays for those cases, not really happy with that
    A table is created for the results display, and a different set of lines is retrieved for each set of different XML setting
    Table is appended to results body
    If Find is clicked again, a new set of results gets appended at the bottom of the body, again
    
    display_results uses a div arg now to separate between many different divs I've added for different display fields
    Add non-functional clear button
    Some minor colouring for error display

--- finder/main.js
+++ finder/main.js
@@ -1,5 +1,19 @@
 
+current_ids = []; // global because needs to assign in fetch
+current_cursor = 1; // current index + 1 of current_ids (which will be accessed on user input)
+
 search = () => {
+	let author = document.getElementById('author').value;
+	if (author == '') {
+		display_results('error', 'author name required.');
+		return 1;
+	} else {
+		display_results('error', ''); // clear above error
+	}
+	let extra = document.getElementById('extra').value;
+	console.log(author);
+	console.log(extra);
+
 	fetch_ids();
 	// fetch_mails();
 
@@ -8,30 +22,45 @@ search = () => {
 fetch_ids = () => {
 		let url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term=Fumihito%20Hirai[Author]+gastroenterology';
 	
-		fetch(url)
-		.then((response) => response.text())
-		.then((text) => {
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(text, 'text/xml');
-			let id_arr_html_col = doc.documentElement.childNodes[3].children
-	
-			let id_list = Array.from(id_arr_html_col);
-			console.log(id_list) // Id objects
-			console.log(id_list[0].textContent) // id as string stripped from <Id> </Id>
-			console.log(typeof(id_list[0])) // object
-			console.log(typeof(id_list[0].textContent)) // string
-	
-			let id_list_strings = id_list.map((i) => {return i.textContent}); // map text content (ids only) to a new array
-			console.log(id_list_strings);
-			// return id_list_strings; can't return since these are async
-			// display_results(id_list_strings.join(', '));
+		try {
+			fetch(url)
+			.then(handle_connection_error)
+			.then((response) => response.text())
+			.then((text) => {
+				display_results('error', '');
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(text, 'text/xml');
+				let id_arr_html_col = doc.documentElement.childNodes[3].children
+		
+				let id_list = Array.from(id_arr_html_col);
+				console.log(id_list) // Id objects
+				console.log(id_list[0].textContent) // id as string stripped from <Id> </Id>
+				console.log(typeof(id_list[0])) // object
+				console.log(typeof(id_list[0].textContent)) // string
+		
+				let id_list_strings = id_list.map((i) => {return i.textContent}); // map text content (ids only) to a new array
+				console.log(id_list_strings);
+				// return id_list_strings; can't return since these are async
+				// display_results(id_list_strings.join(', '));
 
-			console.log(splice_ids_array(id_list_strings));
-		});
+				current_ids = splice_ids_array(id_list_strings);
+				console.log(id_list_strings.length);
+				console.log(current_ids.length);
+				console.log(current_ids[0]);
+				display_results('found-ids', `${id_list_strings.length} articles found.`);
+				display_results('look-through', `Look through ${current_cursor}/${current_ids.length}?`);
+				draw_actions_buttons();
+			});
+		} catch (error) {
+			console.log(error);
+			//display_results('error', 'connection error. re-try please!');
+		}
 };
 
-fetch_mails = () => {
-	let url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id=11079516,11068439,10050046&rettype=xml&retmode=xml'
+// @args: id array passed as joined string with ','
+// e.g. 11079516,11068439,10050046
+fetch_mails = (strung_ids) => {
+	let url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id=${strung_ids}&rettype=xml&retmode=xml`
 	
 	fetch(url)
 	.then((response) => response.text())
@@ -43,39 +72,55 @@ fetch_mails = () => {
 		let emails_arr_html_col = xmldoc.getElementsByTagName('email')
 		let emails_list = Array.from(emails_arr_html_col);
 		console.log(emails_list)
-
-
-		console.log(emails_list[0].parentNode)
-		console.log(emails_list[7].parentNode)
-
-		console.log(emails_list[0].parentNode.nodeName)
-		console.log(emails_list[7].parentNode.parentNode)
-
 		// corresp or contrib 
 		// corresp: direct contact info under author information
 		// contrib: additional mails listed under contributor information
 
 		let corresp_list = [] // corresp>> "information", email
 		let contrib_list = [] // contrib>> name>>surname,givenname, address>>email
+		let contrib_list_one_level = [] // contrib>> name, email
+		let contrib_list_one_level_p = [] // p>> "info", email
+		// FIXME: getting out of hand
 
 		emails_list.forEach(elem => {
 			if (elem.parentNode.nodeName == 'corresp') {
 				corresp_list.push(elem.parentNode);
-			} else {
-				contrib_list.push(elem.parentNode.parentNode)
+			} else if (elem.parentNode.nodeName == 'contrib') { // contrib>> name, email
+					contrib_list_one_level.push(elem.parentNode);
+			} else if (elem.parentNode.nodeName == 'p') { // p>> "info", email
+				contrib_list_one_level_p.push(elem.parentNode);
+			} else { // contrib>> name>>surname,givenname, address>>email
+				contrib_list.push(elem.parentNode.parentNode);
 			}
 		});
 
 		console.log(corresp_list);
 		console.log(contrib_list);
+		console.log(contrib_list_one_level);
+		console.log(contrib_list_one_level_p);
 
-		
+		let output = '<table class=\'results-table\'>';
+		corresp_list.forEach(elem => {
+			output += get_corresp_line(elem);
+		});
+		contrib_list.forEach(elem => {
+			output += get_contrib_line(elem);
+		});
+		contrib_list_one_level.forEach(elem => {
+			output += get_contrib_line_one_level(elem);
+		});
+		contrib_list_one_level_p.forEach(elem => {
+			output += get_contrib_line_one_level_p(elem);
+		});
+		output += '</table>'; // TODO: possibly put this in a div here for better readability when adding multiple tables?
 
+		// TODO: change this with insertAdjacentHTML to prepend
+		document.getElementById('results').innerHTML += output;
 	});
 };
 
-display_results = (text) => {
-	document.getElementById('result').innerHTML = text;
+display_results = (div_id, text) => {
+	document.getElementById(div_id).innerHTML = text;
 };
 
 // split ids array into manageable chunks
@@ -88,11 +133,79 @@ splice_ids_array = (arr) => {
 		split_arr.push(chunk);
 	};
 
-	console.log(split_arr);
 	return split_arr;
 }
+
+draw_actions_buttons = () => {
+	let div = document.getElementById('actions-buttons');
+	div.innerHTML = `
+	<button id='access-next-button' onclick='access_next_set_of_ids()'>Find!</button>
+
+	`
+	// change button text to re-try if pull is unsuccessful
+};
+
+access_next_set_of_ids = () => {
+	// when reaching length, disable button
+	// need to get current values here, then increment
+	// TODO: disable button on click and re-enable when fetch mails is complete?
+	fetch_mails(current_ids[current_cursor - 1].join(','));
+	current_cursor += 1;
+	if (current_ids.length < current_cursor) {
+		document.getElementById('access-next-button').disabled = true;
+	}
+	display_results('look-through', `Look through ${current_cursor}/${current_ids.length}?`);
+};
+
+// corresp obj>> "information", email
+get_corresp_line = (corresp) => {
+	// TODO: possibly split last child initially, and give the rest of the info to first cell, instead of duplicate email
+	return `<tr class='corresp-line'>
+		<td>${corresp.innerHTML}</td>
+		<td>${corresp.lastElementChild.innerHTML}</td>
+	</tr>`;
+};
+
+// contrib>> name>>surname,givenname, address>>email
+// FIXME: need more samples for fixing
+get_contrib_line = (contrib) => {
+	return `<tr class='contrib-line'>
+		<td>${contrib.children[0].innerHTML}</td>
+		<td>${contrib.lastChild.innerHTML}</td>
+	</tr>`;
+};
+
+// contrib>> name, email
+get_contrib_line_one_level = (contrib) => {
+	return `<tr class='contrib-line-one-level'>
+		<td>${contrib.children[0].innerHTML}</td>
+		<td>${contrib.children[1].innerHTML}</td>
+	</tr>`;
+};
+
+// p>> "info", email
+get_contrib_line_one_level_p = (contrib) => {
+	return `<tr class='contrib-line-one-level-p'>
+	<td>${contrib.childNodes[0].nodeValue}</td>
+	<td>${contrib.lastChild.innerHTML}</td>
+</tr>`;
+};
+
+clear = () => {
+
+};
+
+handle_connection_error = (response) => {
+	if (!response.ok) {
+		display_results('error', 'connection error. re-try please!');
+		console.log(error); // FIXME: possibly this gets executed, but didn't have console.log for testing, so maybe display_results error just got overriden at the start of the rest of the functionality
+		throw new Error(response.status);
+	}
+	return response;
+};
 
 	// TODO: have ability to re-try connection if unable to reach for a chunk
 	// also have the option to re-try connection on main IDs fetch
 	// clear functionality for output
 	// on each call append the list of e-mails and info into a different div, spawning on top, so prepend and re-draw? look into .insertAdjacentHTML (https://stackoverflow.com/a/22260849/4085881)
+	// re-try in fetch itself a few times? before giving an option to re-try outside?
